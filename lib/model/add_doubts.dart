d881b240cf06002b868f0202687e7afeebc08e26
abhishekd23 <abhishekdinesh21042001@gmail.com>
2021-06-12 18:12:23 +0530

    Doubts Collection

--- lib/model/add_doubts.dart
+++ lib/model/add_doubts.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_your_tutor/constants/constants.dart';
 import 'package:find_your_tutor/constants/rounded_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+User? user;
 
 class AddDoubts extends StatefulWidget {
   const AddDoubts({Key? key}) : super(key: key);
@@ -11,9 +14,32 @@ class AddDoubts extends StatefulWidget {
 }
 
 class _AddDoubtsState extends State<AddDoubts> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
-  CollectionReference doubts = FirebaseFirestore.instance.collection('users');
+
+  Future<void> getUser() async {
+    user = await _auth.currentUser;
+
+  }
+  DocumentReference currentuser;
+  CollectionReference doubts = FirebaseFirestore.instance
+      .collection('users')
+      .doc(user!.uid)
+      .collection("doubts");
+  FirebaseFirestore.instance
+      .collection('users')
+      .doc(user!.uid).get().then((value) {
+        currentuser = value;
+  });
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUser();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +80,9 @@ class _AddDoubtsState extends State<AddDoubts> {
               RoundedButton(
                 colour: Colors.white,
                 text: "Post",
-                onPressed: () async {},
+                onPressed: () async {
+                  doubts.add({"name":currentuser.})
+                },
               ),
             ],
           ),
