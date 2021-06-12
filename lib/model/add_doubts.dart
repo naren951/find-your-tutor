043411664a5168866af3dca3deb3b9c2f388ee8b
abhishekd23 <abhishekdinesh21042001@gmail.com>
2021-06-12 17:34:01 +0530

    Student Home Page Initial-2

--- lib/model/add_doubts.dart
+++ lib/model/add_doubts.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_your_tutor/constants/constants.dart';
+import 'package:find_your_tutor/constants/rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class AddDoubts extends StatefulWidget {
@@ -11,13 +13,14 @@ class AddDoubts extends StatefulWidget {
 class _AddDoubtsState extends State<AddDoubts> {
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
+  CollectionReference doubts = FirebaseFirestore.instance.collection('users');
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
         child: Padding(
-          padding: EdgeInsets.only(top: 50.0),
+          padding: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
           child: Column(
             children: [
               TextField(
@@ -35,14 +38,23 @@ class _AddDoubtsState extends State<AddDoubts> {
               ),
               TextField(
                 controller: _descriptionController,
-                maxLines: 14,
-                keyboardType: TextInputType.text,
+                maxLines: 10,
+                //expands: true,
+                keyboardType: TextInputType.multiline,
                 decoration: kTextFieldDecoration.copyWith(
                   labelText: "Description",
                   labelStyle: TextStyle(
                     color: Colors.white,
                   ),
                 ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              RoundedButton(
+                colour: Colors.white,
+                text: "Post",
+                onPressed: () async {},
               ),
             ],
           ),
